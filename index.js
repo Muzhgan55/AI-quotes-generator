a4f13b4657fa9f19afb38db28609f20cbc763916
Muzhgan55 <muzhganrahimi8@gmail.com>
2026-03-26 10:57:06 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generateQoute(event) {
+  event.preventDefault();
 
+  new Typewriter("#qoute", {
+    strings: ["The Qoutes will display here"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let qouteFormElement = document.querySelector("#qoute-generator");
+qouteFormElement.addEventListener("submit", generateQoute);
