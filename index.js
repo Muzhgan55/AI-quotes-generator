a54b1588b4ff96b232531686ae3bf9ac8fd00bd6
Muzhgan55 <muzhganrahimi8@gmail.com>
2026-03-27 09:42:54 +0430

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function generateQoute(event) {
-  event.preventDefault();
+let typewriter = new Typewriter("#quote", {
+  autoStart: false,
+  delay: 40,
+});
 
-  new Typewriter("#qoute", {
-    strings: ["The Qoutes will display here"],
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+function displayQuote(response) {
+  typewriter.deleteAll(); // clear previous text
+
+  typewriter.typeString(response.data.answer).start();
 }
 
-let qouteFormElement = document.querySelector("#qoute-generator");
-qouteFormElement.addEventListener("submit", generateQoute);
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#instructions");
+  let apiKey = "bao59c44a03187a19379f00f52aadt46";
+  let prompt = `instructions: Generate English quote about ${instructionInput.value}`;
+  let context =
+    "You are an quote generator expert and love to generate meaningful quote. Make sure to follow the instructions and generate short quote. ";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayQuote);
+}
+
+let quoteFormElement = document.querySelector("#quote-generator");
+quoteFormElement.addEventListener("submit", generateQuote);
